638c3db8af966ec283342f58364abee37b6ead11
salmomascarenhas <salmo.cruz@gmail.com>
2025-06-23 23:34:40 -0300

    feat: add Swagger configuration for API documentation

---
+++ src/config/swagger.ts
@@ -0,0 +1,27 @@
+import { Express } from 'express';
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUi from 'swagger-ui-express';
+
+const options: swaggerJSDoc.Options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Minha Plantinha API',
+      version: '1.0.0',
+      description: 'Documentação da API para o projeto Minha Plantinha.',
+    },
+    servers: [
+      {
+        url: 'http://localhost:3000',
+        description: 'Servidor de Desenvolvimento',
+      },
+    ],
+  },
+  apis: ['./src/routes/*.ts'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+
+export const setupSwagger = (app: Express) => {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+};
