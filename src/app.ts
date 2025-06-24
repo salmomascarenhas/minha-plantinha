12416b78c182d2690912c7e83741b0ebfe694e27
salmomascarenhas <salmo.cruz@gmail.com>
2025-06-24 04:27:01 -0300

    feat: integrate express-ejs-layouts for improved view rendering

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import express from 'express';
+import expressLayouts from 'express-ejs-layouts';
 import path from 'path';
 import { setupSwagger } from './config/swagger';
 import { errorMiddleware } from './errors/errorMiddleware';
@@ -11,11 +12,14 @@ const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
 app.set('views', path.join(__dirname, '..', 'views'));
 app.use(express.static(path.join(__dirname, '..', 'public')));
 
 app.use(express.json());
 
+app.set('layout', 'layout');
 app.use('/', viewRoutes);
 
 app.use('/api', apiRoutes);
