c672ca3fd5deb83fa23b740631e4ce16c201db75
salmomascarenhas <salmo.cruz@gmail.com>
2025-06-24 00:54:37 -0300

    feat: add plant property to Express Request interface for enhanced type support

--- src/types/custom.d.ts
+++ src/types/custom.d.ts
@@ -1,9 +1,11 @@
+import { Plant } from '@prisma/client';
 import { UserData } from '../services/userService';
 
 declare global {
   namespace Express {
     export interface Request {
       user?: UserData & { id: string };
+      plant?: Plant;
     }
   }
 }
