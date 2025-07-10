96e5badb614566d1c640c83bb3755d8ea6b849b6
salmomascarenhas <salmo.cruz@gmail.com>
2025-07-10 03:21:31 -0300

    feat: add ShowcaseSection component to LandingPage and include associated styles

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -2,6 +2,7 @@ import { Box, Divider } from "@mantine/core";
 import { FeaturesSection } from "../components/landing/FeaturesSection";
 import { HeroSection } from "../components/landing/HeroSection";
 import { PublicFooter } from "../components/landing/PublicFooter";
+import { ShowcaseSection } from "../components/landing/ShowcaseSection";
 
 export function LandingPage() {
   return (
@@ -9,6 +10,7 @@ export function LandingPage() {
       <HeroSection />
       <Divider my="md" />
       <FeaturesSection />
+      <ShowcaseSection />
       <PublicFooter />
     </Box>
   );
